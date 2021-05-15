cb89a78c64104f8172fc0298eda8c407943776fa
DonDon2121 <81027157+DonDon2121@users.noreply.github.com>
2021-05-15 11:54:32 -0500

    dc

--- cochranfinal/ViewController.swift
+++ cochranfinal/ViewController.swift
@@ -15,5 +15,23 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func LOGIN(_ sender: UIButton) {
+        
+        
+        func message(){
+            
+
+                let alert = UIAlertController(title: "My Alert", message: "This is an alert.", preferredStyle: .alert)
+
+                alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Default action"), style: .default, handler: { _ in
+
+                NSLog("The \"OK\" alert occured.")
+
+                }))
+
+                self.present(alert, animated: true, completion: nil)
+
+            }
+    }
 }
 
